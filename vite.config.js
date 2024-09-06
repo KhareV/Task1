1b663be1165019ed1108fb9d8d6a478eb0650a8f
Vedant Khare <kharevedant05@gmail.com>
2024-09-06 23:56:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: ['react-loader-spinner'] // Add the module name here
-    }
-  }
-})
+    outDir: 'dist',  // Ensure output directory is set to dist (Vite default)
+  },
+});
