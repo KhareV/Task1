953b457cf08e32e6a7d87872700ef76a6d094dc0
Vedant Khare <kharevedant05@gmail.com>
2024-09-06 23:55:59 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,24 @@
 module.exports = {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}', // Add this line to scan your files
+    './src/**/*.{js,jsx,ts,tsx}', // Scan all necessary files
   ],
   theme: {
     extend: {
       colors: {
-        // Add your custom colors here
         customBlue: '#3B5987', // Example custom blue color
         customGray: '#64748b', // Example custom gray color
         customOrange: '#fb923c', // Example custom orange color
+      },
+      screens: {
+        sm: '640px',   // Small screens
+        md: '768px',   // Medium screens
+        lg: '1024px',  // Large screens
+        xl: '1280px',  // Extra large screens
+      },
+      width: {
+        'custom-small': '100%',    // Full width for small screens
+        'custom-medium': '50%',    // Medium width for larger screens
+        'custom-large': '30%',     // Smaller width for large screens
       },
     },
   },
